be72443167ba3a0136c57e7935669e738f08c225
charaekeow <charaekeow11@gmail.com>
2024-03-10 00:37:37 +0800

    feat: add logger and x-response time middleware

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,23 @@ import Koa from 'koa';
 const PORT = 8000;
 const app = new Koa();
 
-app.use((ctx) => {
+// logger
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+	await next();
+	const responseTime = ctx.response.get('X-Response-Time');
+	console.log(`${ctx.method} ${ctx.url} - ${responseTime}`);
+});
+
+// x-response-time
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+	const startTime = Date.now();
+	await next();
+	const responseTime = Date.now() - startTime;
+	ctx.set('X-Response-Time', `${responseTime}ms`);
+});
+
+// response
+app.use(async (ctx: Koa.Context) => {
 	ctx.body = 'Hello world';
 });
 
